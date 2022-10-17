94274be26a2c056832746a8a652a3963492a801e
luckelucky <officialluckelucky@gmail.com>
2022-10-17 14:16:50 +0100

    3Qual-32SETeamBracket

--- scripts/match2conversion/bracket_helper.py
+++ scripts/match2conversion/bracket_helper.py
@@ -112,6 +112,7 @@ bracketAlias = {
 	'3Qual-12SETeamBracket': '8-2Q-4-1Q',
 	'3Qual-16DETeamBracket': '16-2Q-U-8L4DSL2DS-1Q',
 	'3Qual-16SETeamBracket': '16-2Q-2-1Q',
+	'3Qual-32SETeamBracket': '32-2Q-2-1Q',
 	'12Qual-16DETeamBracket': '2-2-2-2-2-2-2-2-U-2-2-2-2',
 	'16Qual-32DETeamBracket' : '32-8Q-U-16L8D-8Q',
 	'16Qual-32SETeamBracket' : '32-16Q',
