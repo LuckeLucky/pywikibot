ddc14548cc4f10b3abf1212ff241e26a02ce1152
LuckeLucky <officialluckelucky@gmail.com>
2024-01-15 15:10:09 +0000

    add lowercase to random

--- scripts/match2/commons/utils.py
+++ scripts/match2/commons/utils.py
@@ -2,5 +2,5 @@ import random
 import string
 
 def generateId():
-	ran = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 10))
+	ran = ''.join(random.choices(string.ascii_uppercase + string.digits + string.ascii_lowercase, k = 10))
 	return ran
