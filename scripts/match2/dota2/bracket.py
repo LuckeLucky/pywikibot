3bf450fc7ba733e796b4334530f79afbc54ee1b0
LuckeLucky <officialluckelucky@gmail.com>
2024-02-17 14:53:15 +0000

    dota2

---
+++ scripts/match2/dota2/bracket.py
@@ -0,0 +1,13 @@
+from ..commons.bracket import Bracket as commonsBracket
+from ..commons.opponent import Opponent, TeamOpponent
+
+class Bracket(commonsBracket):
+	def getTeamOpponent(self, key: str, scoreKey: str) -> Opponent:
+		name = self.template.getValue(key + 'team')
+		score = self.template.getValue(key + scoreKey)
+		if name:
+			return TeamOpponent(name, score)
+		csName = self.template.getValue(key)
+		if csName:
+			return Opponent(csName, score)
+		return TeamOpponent()
---
+++ scripts/match2/dota2/match.py
@@ -0,0 +1,65 @@
+from ..commons.template import Template
+from ..commons.match import Match as commonsMatch, STREAMS
+
+from .map import Map
+
+MAX_NUMBER_OF_MAPS = 10
+DOTA_PARAMS = STREAMS + [
+	'comment',
+	'walkover',
+	'vod',
+	'mvp',
+	'mvppoints',
+	'preview',
+	'lrthread',
+	'reddit',
+	'bestgg',
+	'interview',
+	'review',
+	'recap'
+]
+
+class Match(commonsMatch):
+	def getMaps(self):
+		for mapIndex in range(1, MAX_NUMBER_OF_MAPS):
+			mapTemplate = self.template.getNestedTemplate('match' + str(mapIndex))
+			mapWinner = self.template.getValue('map' + str(mapIndex) + 'win')
+			if mapTemplate is None and mapWinner:
+				mapTemplate = Template.createFakeTemplate()
+			if not mapTemplate:
+				break
+			mapTemplate = Template(mapTemplate)
+			if not mapTemplate.getValue('win'):
+				mapTemplate.add('win', mapWinner)
+			newMap = Map(mapIndex, mapTemplate)
+			self.maps.append(newMap)
+
+	def __str__(self) -> str:
+		opponent1 = self.opponents[0]
+		opponent2 = self.opponents[1]
+		out = ("{{Match2\n" +
+			f"|opponent1={str(opponent1)}\n" +
+			f"|opponent2={str(opponent2)}\n" +
+			f"|date={self.template.getValue('date')}\n"
+			f"|finished={self.template.getValue('finished')}\n")
+
+		winner = self.template.getValue('winner')
+		if winner:
+			out += f"|winner={winner}\n"
+
+		for key, value in self.template.iterateByItemsMatch(DOTA_PARAMS):
+			out += f"|{key}={value}\n"
+
+		for key, value in self.template.iterateByPrefix('vodgame'):
+			out += f"|{key}={value}\n"
+
+		for key, value in self.template.iterateByPrefix('matchid'):
+			out += f"|{key}={value}\n"
+
+		for matchMap in self.maps:
+			index = matchMap.index
+			out += f"|map{index}={str(matchMap)}\n"
+
+		out += "}}"
+
+		return out
