98cc295b6128cc0ba634e4f843e6984a07bbab47
LuckeLucky <officialluckelucky@gmail.com>
2024-03-27 09:15:16 +0000

    update family

--- pywikibot/families/liquipedia_family.py
+++ pywikibot/families/liquipedia_family.py
@@ -24,7 +24,7 @@ class Family(family.Family):
         'brawlhalla': 'liquipedia.net',
         'brawlstars': 'liquipedia.net',
         'callofduty': 'liquipedia.net',
-        'callofduty': 'liquipedia.net',
+        'clashofclans': 'liquipedia.net',
         'clashroyale': 'liquipedia.net',
         'commons': 'liquipedia.net',
         'counterstrike': 'liquipedia.net',
@@ -33,15 +33,19 @@ class Family(family.Family):
         'dota2': 'liquipedia.net',
         'fifa': 'liquipedia.net',
         'fighters': 'liquipedia.net',
+        'formula1': 'liquipedia.net',
         'fortnite': 'liquipedia.net',
         'freefire': 'liquipedia.net',
         'halo': 'liquipedia.net',
         'hearthstone': 'liquipedia.net',
         'heroes': 'liquipedia.net',
+        'illuvium': 'liquipedia.net',
         'leagueoflegends': 'liquipedia.net',
         'magic': 'liquipedia.net',
         'mobilelegends': 'liquipedia.net',
         'naraka': 'liquipedia.net',
+        'omegastrikers': 'liquipedia.net',
+        'osu': 'liquipedia.net',
         'overwatch': 'liquipedia.net',
         'paladins': 'liquipedia.net',
         'pokemon': 'liquipedia.net',
@@ -53,6 +57,7 @@ class Family(family.Family):
         'runeterra': 'liquipedia.net',
         'sideswipe': 'liquipedia.net',
         'simracing': 'liquipedia.net',
+        'smite': 'liquipedia.net',
         'smash': 'liquipedia.net',
         'splatoon': 'liquipedia.net',
         'splitgate': 'liquipedia.net',
@@ -61,14 +66,17 @@ class Family(family.Family):
         'starcraft': 'liquipedia.net',
         'starcraft2': 'liquipedia.net',
         'stormgate': 'liquipedia.net',
-        'tft': 'liquipedia.net',
         'teamfortress': 'liquipedia.net',
+        'tetris': 'liquipedia.net',
+        'tft': 'liquipedia.net',
         'trackmania': 'liquipedia.net',
         'underlords': 'liquipedia.net',
         'valorant': 'liquipedia.net',
         'warcraft': 'liquipedia.net',
         'wildrift': 'liquipedia.net',
+        'worldoftanks': 'liquipedia.net',
         'worldofwarcraft': 'liquipedia.net',
+        'zula': 'liquipedia.net',
 
         'sportscommons': 'sports.liquipedia.net',
         'football': 'sports.liquipedia.net',
@@ -83,10 +91,6 @@ class Family(family.Family):
             return '/commons'
         else:
             return '/' + code
-
-    @deprecated('APISite.version()')
-    def version(self, code):
-        return u'1.35.5'
 
     def protocol(self, code):
         if code == 'fts':
