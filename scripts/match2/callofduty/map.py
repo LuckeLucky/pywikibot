b74009f3a929402f13b66a60dbd582fdcbbdfc10
LuckeLucky <officialluckelucky@gmail.com>
2024-12-14 23:52:58 +0000

    callofduty

--- scripts/match2/callofduty/map.py
+++ scripts/match2/callofduty/map.py
@@ -1,32 +1,33 @@
+from typing import List
 from ..commons.map import Map as commonsMap
 
 SKIP = 'skip'
 
 class Map(commonsMap):
+	def generateString(self, params: List[str]) -> str:
+		return super().generateTemplateString(params, templateId = 'Map', indent = '', end = '}}')
+
 	def __str__(self) -> str:
 		playedMap = self.getValue(self.prefix)
 		winner = self.getValue(self.prefix + 'win')
 		if not winner and playedMap:
 			winner = SKIP
-		out = (
-			'{{Map' +
-			f'|map={playedMap}' +
-			self.printParam(self.prefix + 'type', 'mode', True)
-		)
+
+		out = [
+			('map', playedMap),
+			('mode', self.getValue(self.prefix + 'type'), True)
+		]
 
 		score = self.getValue(self.prefix + 'score')
 		if score and '-' in score:
 			splitScore = score.split('-', 1)
-			out = (
-				out +
-				f'|score1={splitScore[0]}'
-				f'|score2={splitScore[1]}'
-			)
+			out.extend([
+				('score1', splitScore[0]),
+				('score2', splitScore[1])
+			])
+		out.extend([
+			('winner', winner),
+			('vod', self.getValue('vodgame' + str(self.index)))
+		])
 
-		out = (
-			out +
-			f'|winner={winner}' +
-			self.printParam('vodgame' + str(self.index), 'vod', True)
-		)
-
-		return out + '}}'
+		return self.generateString([out])
